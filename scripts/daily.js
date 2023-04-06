9af71acefecf9f9336c62f61e3c366ce32430374
vnnm <vennam404@gmail.com>
2023-04-07 00:13:00 +0530

    done with the base essentials of part 2

--- scripts/daily.js
+++ scripts/daily.js
@@ -1,136 +1,123 @@
-
 const data = [
-    {
-        class: "Class 1",
-        start: new Date(2023, 3, 6, 8),
-        end: new Date(2023, 3, 6, 16),
-        tasks: [
-            { name: "Task 1.1", start: new Date(2023, 3, 6, 8), end: new Date(2023, 3, 6, 10) },
-            { name: "Task 1.2", start: new Date(2023, 3, 6, 11), end: new Date(2023, 3, 6, 13) },
-            { name: "Task 1.3", start: new Date(2023, 3, 6, 14), end: new Date(2023, 3, 6, 16) }
-        ]
-    },
-    {
-        class: "Class 2",
-        start: new Date(2023, 3, 6, 9),
-        end: new Date(2023, 3, 6, 17),
+  {
+    class: "Class 1",
+    start: new Date(2023, 3, 6, 8),
+    end: new Date(2023, 3, 6, 16),
+    tasks: [
+      { name: "Task 1.1", start: new Date(2023, 3, 6, 8), end: new Date(2023, 3, 6, 10) },
+      { name: "Task 1.2", start: new Date(2023, 3, 6, 11), end: new Date(2023, 3, 6, 13) },
+      { name: "Task 1.3", start: new Date(2023, 3, 6, 14), end: new Date(2023, 3, 6, 16) }
+    ]
+  },
+  {
+    class: "Class 2",
+    start: new Date(2023, 3, 6, 9),
+    end: new Date(2023, 3, 6, 17),
 
-        tasks: [
-            { name: "Task 2.1", start: new Date(2023, 3, 6, 9), end: new Date(2023, 3, 6, 11) },
-            { name: "Task 2.2", start: new Date(2023, 3, 6, 12), end: new Date(2023, 3, 6, 14) },
-            { name: "Task 2.3", start: new Date(2023, 3, 6, 15), end: new Date(2023, 3, 6, 17) }
-        ]
-    },
-    {
-        class: "Class 3",
-        start: new Date(2023, 3, 6, 10),
-        end: new Date(2023, 3, 6, 19),
-        tasks: [
-            { name: "Task 3.1", start: new Date(2023, 3, 6, 10), end: new Date(2023, 3, 6, 12) },
-            { name: "Task 3.2", start: new Date(2023, 3, 6, 13), end: new Date(2023, 3, 6, 16) },
-            { name: "Task 3.3", start: new Date(2023, 3, 6, 17), end: new Date(2023, 3, 6, 19) }
-        ]
-    }
+    tasks: [
+      { name: "Task 2.1", start: new Date(2023, 3, 6, 9), end: new Date(2023, 3, 6, 11) },
+      { name: "Task 2.2", start: new Date(2023, 3, 6, 12), end: new Date(2023, 3, 6, 14) },
+      { name: "Task 2.3", start: new Date(2023, 3, 6, 15), end: new Date(2023, 3, 6, 17) }
+    ]
+  },
+  {
+    class: "Class 3",
+    start: new Date(2023, 3, 6, 10),
+    end: new Date(2023, 3, 6, 19),
+    tasks: [
+      { name: "Task 3.1", start: new Date(2023, 3, 6, 10), end: new Date(2023, 3, 6, 12) },
+      { name: "Task 3.2", start: new Date(2023, 3, 6, 13), end: new Date(2023, 3, 6, 16) },
+      { name: "Task 3.3", start: new Date(2023, 3, 6, 17), end: new Date(2023, 3, 6, 19) }
+    ]
+  }
 ];
 
+const elementWidth = 1000;
+const elementHeight = 600;
 
 // Define the dimensions of the chart
 const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-const width = 1000 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+const width = elementWidth - margin.left - margin.right;
+const height = elementHeight - margin.top - margin.bottom;
 
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleBand().range([0, height]);
 
 const svg = d3
-    .select("#svg_one")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+  .select("#svg_one")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // flatten the subtasks to be able to draw them on separate rows
 const flattenedTasks = data.reduce((acc, curr) => {
-    acc.push({ class: curr.class, task: "", start: curr.start, end: curr.end, isTask: false , color : null});
-    curr.tasks.forEach((task) => {
-        acc.push({ class: curr.class, task: task.name, start: task.start, end: task.end, isTask: true });
-    });
-    return acc;
+  acc.push({ class: curr.class, task: "", start: curr.start, end: curr.end, isTask: false, color: null });
+  curr.tasks.forEach((task) => {
+    acc.push({ class: curr.class, task: task.name, start: task.start, end: task.end, isTask: true });
+  });
+  return acc;
 }, []);
 
-console.log(flattenedTasks)
+// console.log(flattenedTasks);
 
 // set domain for the scales
 y.domain(flattenedTasks.map((d) => (d.class + d.task)));
 x.domain([
-    d3.min(flattenedTasks, (d) => d.start),
-    d3.max(flattenedTasks, (d) => d.end),
+  d3.min(flattenedTasks, (d) => d.start),
+  d3.max(flattenedTasks, (d) => d.end),
 ]);
 
 // draw x-axis
-svg
-    .append("g")
-    .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x).ticks(d3.timeHour.every(1)))
-    .select(".domain")
-    .remove();
+svg.append("g")
+  .attr("transform", `translate(0,${height})`)
+  .call(d3.axisBottom(x).ticks(d3.timeHour.every(1)))
+  .select(".domain")
+  .remove();
 
 // draw y-axis
 svg.append("g").call(d3.axisLeft(y).tickSize(0));
 
-
 const groups = svg
-    .selectAll(".group")
-    .data(flattenedTasks)
-    .enter()
-    .append("g")
-    .attr("class", "group")
-    .attr("transform", (d) => `translate(0, ${y(d.class + d.task)})`);
+  .selectAll(".group")
+  .data(flattenedTasks)
+  .enter()
+  .append("g")
+  .attr("class", "group")
+  .attr("transform", (d) => `translate(0, ${y(d.class + d.task)})`);
 
-
-
-
+// draw class/task bars
 groups
-    .filter((d) => !d.isTask)
-    .append("rect")
-    .attr("class", "class")
-    .attr("x", (d) => x(d.start))
-    .attr("y", 0)
-    .attr("width", (d) => x(d.end) - x(d.start))
-    .attr("height", y.bandwidth())
-.style("fill", (d) => getColor(d));
+  .filter((d) => !d.isTask)
+  .append("rect")
+  .attr("class", "class")
+  .attr("x", (d) => x(d.start))
+  .attr("y", 0)
+  .attr("width", (d) => x(d.end) - x(d.start))
+  .attr("height", y.bandwidth())
+  .style("fill", (d) => getColor(d));
 
 // draw the subtask bars
 groups
-    .filter((d) => d.isTask)
-    .append("rect")
-    .attr("class", "task")
-    .attr("x", (d) => x(d.start))
-    .attr("y", y.bandwidth() / 4)
-    .attr("width", (d) => x(d.end) - x(d.start))
-    .attr("height", y.bandwidth() / 2)
-.style("fill", (d) => getTaskColor(d.class));
+  .filter((d) => d.isTask)
+  .append("rect")
+  .attr("class", "task")
+  .attr("x", (d) => x(d.start))
+  .attr("y", y.bandwidth() / 4)
+  .attr("width", (d) => x(d.end) - x(d.start))
+  .attr("height", y.bandwidth() / 2)
+  .style("fill", (d) => getColor(d.class));
 
 const color_map = []
 
 function getColor(d) {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
 
-    
+  const color = `#${r.toString(16).padStart(2, '0')}` +
+    `${g.toString(16).padStart(2, '0')}` +
+    `${b.toString(16).padStart(2, '0')}`;
 
-    const color = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
-    
-    return color;
-
+  return color;
 }
-
-function getTaskColor(c) {
-    
-
-}
-
-
-
-
---
+++ scripts/weekly.js
@@ -0,0 +1,151 @@
+const data = await d3.csv('/data/weekly.csv');
+data.forEach(item => {
+  const startTimeParts = item.start_time.split(':');
+  item.start_hour = parseInt(startTimeParts[0]);
+  item.start_minute = parseInt(startTimeParts[1]);
+  item.start_minutes = item.start_hour * 60 + item.start_minute;
+  delete item.start_time;
+  // item.start_time = new Date(2023, 3, 6, item.start_hour, item.start_minute);
+  
+  const endTimeParts = item.end_time.split(':');
+  item.end_hour = parseInt(endTimeParts[0]);
+  item.end_minute = parseInt(endTimeParts[1]);
+  item.end_minutes = item.end_hour * 60 + item.end_minute;
+  delete item.end_time;
+
+  item.duration = item.end_minutes - item.start_minutes;
+  // item.end_time = new Date(2023, 3, 6, item.end_hour, item.end_minute);
+});
+
+const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+const svg = d3.select('#svg_two');
+
+// console.log(data);
+
+// viewbox 0 0 700 600
+function drawSVG() {
+  const width = 700;
+  const height = 600;
+
+  const leftMargin = width * 0.05;
+  const rightMargin = width * 0.05;
+  const topMargin = height * 0.00;
+  const bottomMargin = height * 0.00;
+
+  const w = width - leftMargin - rightMargin;
+  const h = height - topMargin - bottomMargin;
+
+  svg.append('svg')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('id', 'svg_two_inner');
+
+  const svgData = d3.select('#svg_two_inner')
+    .attr('x', leftMargin)
+    .attr('y', topMargin);
+
+  // draw y lines
+  for(let i = 0; i < days.length + 1; i++) {
+    svgData.append('line')
+      .attr('x1', (w / days.length) * i)
+      .attr('x2', (w / days.length) * i)
+      .attr('y1', 0)
+      .attr('y2', h)
+      .attr('style', 'stroke:grey;stroke-width:1');
+
+    if (i >= 1 && i <= 7) {
+      const day = days[i - 1];
+
+      svgData.append('text')
+        .attr('x', w / days.length * (i - 1) + 5)
+        .attr('y', 15)
+        .text(day.substring(0, 3).toUpperCase());
+    }
+  }
+
+  // draw x lines (time 8am to 6pm => 8:00 to 18:00)
+  const rows = (18 - 8 + 1) + 1;
+  for(let i = 0; i < rows - 1; i++) {
+    svgData.append('line')
+      .attr('x1', 0)
+      .attr('x2', w)
+      .attr('y1', (h / rows) * (i + 1))
+      .attr('y2', (h / rows) * (i + 1))
+      .attr('style', 'stroke:grey;stroke-width:1');
+
+    let time = `${i + 8}:00`;
+    // append times
+    svg.append('text')
+      .attr('x', 7)
+      .attr('y', (h / rows) * (i + 1) + 8)
+      .text(time);
+  }
+
+  // draw boxes
+  for(let i = 0; i < days.length; i++) {
+    let day = days[i];
+
+    const daywiseData = data.filter(item => item.day === day);
+    daywiseData.sort((a, b) => {
+      if (a.start_hour < b.start_hour) return -1;
+      if (a.start_hour > b.start_hour) return 1;
+
+      if (a.start_minute < b.start_minute) return -1;
+      if (a.start_minute > b.start_minute) return 1;
+
+      return 0;
+    });
+
+    function overlappingGroups() {
+      let groups = [];
+      let group = [];
+      let latest = -1;
+      for(let item of daywiseData) {
+        if (item.start_minutes < latest) {
+          group.push(item);
+          latest = Math.max(latest, item.start_minutes + item.duration)
+        } else {
+          if (group.length > 0)
+            groups.push(group);
+
+          group = [item];
+          latest = item.start_minutes + item.duration;
+        }
+      }
+      groups.push(group);
+
+      return groups;
+    }
+
+    // find groups that overlap and display them
+    const groups = overlappingGroups();
+    for(let k = 0; k < groups.length; k++) {
+      let group = groups[k];
+      // (w / days.length)
+      const space = w / days.length;
+      const ww = space / group.length;
+      const start = h / rows;
+      const leftover = h - 2 * start;
+      const padding = 2; // pixels
+
+      // (h / rows) <- 1 hour, for item.duration / 60 hours
+      const totalPaddingSpace = (group.length + 1) * padding;
+      const ww2 = (space - totalPaddingSpace) / group.length;
+      for(let j = 0; j < group.length; j++) {
+        const item = group[j];
+        console.log(item.start_minutes, (18 * 60 - 8 * 60), leftover);
+
+        svgData.append('rect')
+          .attr('x', space * i + ww2 * j + padding * (j + 1))
+          .attr('y', start + (item.start_minutes - 8 * 60) / (18 * 60 - 8 * 60) * (leftover) + padding)
+          .attr('width', ww2)
+          .attr('height', (item.duration / 60) * (h / rows) - 2 * padding)
+          .attr('fill', 'red');
+      }
+    }
+
+    console.log(groups);
+  }
+}
+
+drawSVG();
